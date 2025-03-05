b1e9cccf0cf5cc26b494df769001a2b3f62ad9f8
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-03-05 23:33:27 +0000

    Sync LeetCode submission Runtime - 8 ms (38.40%), Memory - 19.2 MB (97.64%)

--- solutions/0169-majority-element/solution.py
+++ solutions/0169-majority-element/solution.py
@@ -1,15 +1,56 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        element = None
-        count = 0
-        
-        # Step 1: Find the potential majority element
+   
+        # Brute force approach which will give time limit exceeded error
+        #time complexity o(n2)
+
+        # for num in nums:
+        #     freq=0
+        #     for i in nums:
+        #         if num == i:
+        #             freq+=1
+            
+        #     if freq > (len(nums)//2):
+        #         return num
+
+        # using sorting method
+        #time complexity o(nlogn) + o(n)
+
+        # nums.sort()
+        # freq=1
+        # for i in range(len(nums)):
+        #     if nums[i]==nums[i-1]:
+        #         freq+=1
+        #     else:
+        #         freq=1
+        #     if freq > (len(nums)//2):
+        #         return nums[i]
+            
+        # return -1
+
+        # Moore's voting algorithm otimize apporoach
+        freq=0
+        storage=0
+        for i in range(len(nums)):
+            if freq==0:
+                storage= nums[i]
+            if storage == nums[i]:
+                freq+=1
+            else:
+                freq-=1
+        count=0
         for num in nums:
-            if count == 0:
-                element = num
-            count += (1 if num == element else -1)
+            if num == storage:
+                count+=1
+        if count > (len(nums)//2):
+            return storage
+        else: 
+            return -1
+
+        return storage
+
+
+
+                   
         
-        # Step 2: Return the potential majority element
-        # No verification step is needed since the problem guarantees that a majority element always exists.
-        return element
 
