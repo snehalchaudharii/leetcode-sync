78982854ab5845e83cdc415deee6360f58b5b9fc
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-03-06 23:24:29 +0000

    Sync LeetCode submission Runtime - 27 ms (48.30%), Memory - 23.4 MB (41.62%)

---
+++ solutions/0238-product-of-array-except-self/solution.py
@@ -0,0 +1,47 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # brute force approach Time Limit Exceeded
+        # ans=[]
+        # for i in range(len(nums)):
+        #     prod=1
+        #     for j in range(len(nums)):
+        #         if i!=j:
+        #             prod *= nums[j]
+        #     ans.append(prod)
+        # return ans
+
+        # Optimized method by calculating prefix and sufficx before hand
+        # n= len(nums)
+        # ans= [1]*n
+        # # to calculate prefix
+        # prefix=[1]*n
+        # for i in range(1,n):
+        #     prefix[i]= prefix[i-1]*nums[i-1]
+        
+        # # to calculate suffix
+        # suffix=[1]*n
+        # for i in range(n-2, -1, -1):
+        #     suffix[i]= suffix[i+1]*nums[i+1]
+        
+        # for i in range(len(nums)):
+        #     ans[i]= prefix[i]*suffix[i]
+        
+        # return ans
+
+        # To save the space
+        n= len(nums)
+        ans= [1]*n
+        # to calculate prefix
+        prefix=1
+        for i in range(1,n):
+            ans[i]= ans[i-1]*nums[i-1]
+        # to calculate suffix
+        suffix=1
+        for i in range(n-2, -1, -1):
+            suffix *= nums[i+1]
+            ans[i] *= suffix
+        
+        return ans
+           
+
+        
