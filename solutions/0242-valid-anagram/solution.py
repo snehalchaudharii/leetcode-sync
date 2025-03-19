e20d27b668903b5116f33a89f05988561299c63e
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-03-19 05:23:09 +0000

    Sync LeetCode submission Runtime - 15 ms (43.43%), Memory - 17.8 MB (64.33%)

---
+++ solutions/0242-valid-anagram/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # return sorted(s)== sorted(t) TC- NlogN
+
+
+# TC == O(n)
+        if len(s)!= len(t):
+            return False
+        freq={}
+        for i in range(len(s)):
+# It adds 1 to the count of s[i] in the dictionary freq.
+# If s[i] is not already in freq, it initializes freq[s[i]] to 0 before adding 1.
+            freq[s[i]]= freq.get(s[i], 0) + 1
+            freq[t[i]]= freq.get(t[i], 0) - 1
+        return all( value== 0 for value in freq.values())
+
+
