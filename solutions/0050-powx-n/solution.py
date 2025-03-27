08f8ad417cbf25e646b3c6f2da71fff653750203
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-03-27 20:17:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (11.52%)

--- solutions/0050-powx-n/solution.py
+++ solutions/0050-powx-n/solution.py
@@ -2,27 +2,27 @@ class Solution:
     def myPow(self, x: float, n: int) -> float:
         # using binary exponentioal form basically squareing
         result=1
-
-        if n==0:
+        bindaryForm = n
+        if bindaryForm==0:
             return 1.0
         if x==0:
             return 0.0
         if x==1: 
             return 1.0
-        if x== -1 and n%2==0:
+        if x== -1 and bindaryForm%2==0:
             return 1.0
-        if x== -1 and n%2!=0:
+        if x== -1 and bindaryForm%2!=0:
             return -1.0
 
-        if n<0:
+        if bindaryForm <0:
             x= 1/x
-            n= -n
+            bindaryForm= -bindaryForm
         
-        while n>0:
-            if n%2 == 1:
+        while bindaryForm >0:
+            if bindaryForm%2 == 1:
                 result *= x
             x*=x
-            n= n//2
+            bindaryForm= bindaryForm//2
         
         return result
  
