f2c9b5cfbeb04b95dfb5aadad5445f4d5a054d0e
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-04-03 21:27:06 +0000

    Sync LeetCode submission Runtime - 23 ms (32.16%), Memory - 18 MB (34.39%)

--- solutions/0003-longest-substring-without-repeating-characters/solution.py
+++ solutions/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,5 +1,12 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        # Time Complexity: O(n)
+        # Each character is added to longestStr once and removed at most once.
+        # Every right move or left move is done in O(1), leading to O(n).
+        # Space Complexity: O(min(n, 26))
+        # The set stores unique characters, with at most 26 lowercase letters in English.
+        # In the worst case, if all characters are unique, the set has n elements.
+
         count=0
         longstr= set()
         left=0
