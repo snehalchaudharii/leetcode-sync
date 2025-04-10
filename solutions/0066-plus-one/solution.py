1ec5bfbcb8405e5a1ab97b90b8f6e95d79341b0d
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-04-10 23:30:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (50.64%)

---
+++ solutions/0066-plus-one/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        # TC O(n) SC O(1)
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i]<9:
+                digits[i]+=1
+                return digits
+            digits[i]=0
+        return [1]+digits
+
+
+
+
+        # TC O(n) SC O(n)
+        # num = 0
+        # for dig in digits:
+        #     num = num * 10 + dig
+        
+        # # Increment by 1
+        # num += 1
+        
+        # # Convert back to list of digits
+        # ans = []
+        # while num > 0:
+        #     ans.insert(0, num % 10)
+        #     num //= 10
+        # return ans
