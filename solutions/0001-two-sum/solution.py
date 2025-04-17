81f5f9a1fdd94d173bf2ad262041aa6d35f0c6b8
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-04-17 04:15:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (11.51%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -1,7 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mp={}
-
         for index, num in enumerate(nums):
             complement= target-num
 
@@ -9,5 +8,6 @@ class Solution:
                 return [mp[complement], index]
             mp[num]=index
         return []
+
 
             
