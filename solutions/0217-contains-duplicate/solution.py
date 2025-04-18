ca74ad78c671b7ce5e0dc762b2ef31715a5a10f6
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-04-18 04:32:46 +0000

    Sync LeetCode submission Runtime - 24 ms (15.30%), Memory - 34.9 MB (17.45%)

---
+++ solutions/0217-contains-duplicate/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        freq={}
+        for num in nums:
+            if num in freq:
+                return True
+            freq[num]= freq.get(num, 0)+1
+        return False
