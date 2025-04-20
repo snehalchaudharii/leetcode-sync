b6aee163ef719308e66d5a860c767a7c8566269c
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-04-20 21:37:21 +0000

    Sync LeetCode submission Runtime - 590 ms (51.69%), Memory - 20.7 MB (38.67%)

--- solutions/0015-3sum/solution.py
+++ solutions/0015-3sum/solution.py
@@ -2,7 +2,6 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         result=[]
-
         for i in range(len(nums)-2):
 
             if i>0 and nums[i]==nums[i-1]:
@@ -14,22 +13,22 @@ class Solution:
             while left< right:
                 total= nums[i]+nums[left]+nums[right]   
 
-                if total==0:
-                    result.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left]==nums[left+1]:
-                        left+=1
-                    while left<right and nums[right]==nums[right-1]:
-                        right-=1
+                if total<0:
+                    left+=1
                     
-                    left+=1
+                elif total>0:
                     right-=1
-                
-                elif total< 0:
-                    left+=1
                 else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    left+=1
                     right-=1
+                    
+                    while (left<right and nums[left]==nums[left-1]):
+                        left+=1
+                
                 
         return result
 
                                      
-
+# TC O(NlogN+ N^2)
+# SC O(Unique triplet)
