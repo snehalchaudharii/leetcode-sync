309cb1fe4e23083f4266eb2cb6fe91853c323296
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-04-23 04:46:02 +0000

    Sync LeetCode submission Runtime - 313 ms (32.00%), Memory - 17.6 MB (64.42%)

--- solutions/1013-fibonacci-number/solution.py
+++ solutions/1013-fibonacci-number/solution.py
@@ -1,6 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
-       prev, curr= 0, 1
-       for i in range(n):
-        prev, curr= curr, prev+curr
-       return prev
+    #    prev, curr= 0, 1
+    #    for i in range(n):
+    #     prev, curr= curr, prev+curr
+    #    return prev
+
+        if n<=1:
+            return n
+        return self.fib(n-1) + self.fib(n-2)
