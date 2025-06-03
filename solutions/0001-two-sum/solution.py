54ba0ffc85e3fbf9d46e4b69c795fd172c23c522
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-06-03 23:36:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.1 MB (24.06%)

--- solutions/0001-two-sum/solution.py
+++ solutions/0001-two-sum/solution.py
@@ -1,13 +1,23 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mp={}
-        for index, num in enumerate(nums):
-            complement= target-num
-
-            if complement in mp:
-                return [mp[complement], index]
-            mp[num]=index
+        freq={}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in freq:
+                return [i, freq[complement]]
+            freq[num]=i
         return []
+
+
+
+        # mp={}
+        # for index, num in enumerate(nums):
+        #     complement= target-num
+
+        #     if complement in mp:
+        #         return [mp[complement], index]
+        #     mp[num]=index
+        # return []
 
 
             
