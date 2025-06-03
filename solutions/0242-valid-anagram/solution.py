93321cc9becdb68b3b1479b3af8c576199147ebe
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-06-03 23:23:20 +0000

    Sync LeetCode submission Runtime - 11 ms (72.67%), Memory - 17.8 MB (63.77%)

--- solutions/0242-valid-anagram/solution.py
+++ solutions/0242-valid-anagram/solution.py
@@ -2,15 +2,53 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # return sorted(s)== sorted(t) TC- NlogN
 
-# TC == O(n)
+# Still O(n) time and space, but avoids building two full dictionaries.
+        freq={}
+
         if len(s)!= len(t):
             return False
-        freq={}
-        for i in range(len(s)):
+
+        for ch in s:
+            freq[ch]= freq.get(ch, 0)+1
+        
+        for ch in t:
+            if ch not in freq:
+                return False
+            
+            freq[ch]-=1
+
+            if freq[ch]<0:
+                return False
+        return True
+
+
+
+        # freqS={}
+        # freqT={}
+        # if len(s)!=len(t):
+        #     return False
 # It adds 1 to the count of s[i] in the dictionary freq.
 # If s[i] is not already in freq, it initializes freq[s[i]] to 0 before adding 1.
-            freq[s[i]]= freq.get(s[i], 0) + 1
-            freq[t[i]]= freq.get(t[i], 0) - 1
-        return all( value== 0 for value in freq.values())
+        # for i in range(len(s)):
+        #     freqS[s[i]]= freqS.get(s[i], 0)+1
+        #     freqT[t[i]]= freqT.get(t[i], 0)+1       
+        # return freqS == freqT
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
