5d4abb40d0c759ce0c663ff571e1805af756ba87
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-06-04 01:54:00 +0000

    Sync LeetCode submission Runtime - 21 ms (23.52%), Memory - 22.7 MB (13.25%)

--- solutions/0049-group-anagrams/solution.py
+++ solutions/0049-group-anagrams/solution.py
@@ -1,22 +1,20 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # time complexity as O(m * n log n) space complexity O(m * n)
-        # anagrams = defaultdict(list)
-        # for s in strs:
-        #     key = "".join(sorted(s))
-        #     anagrams[key].append(s)
-        # return list(anagrams.values())
+
 
         # TC: O(n * k) SC: O(nk)
+        # N = Number of words
+# k = Length of the longest word (for building frequency array)
 
+# The defaultdict(list) ensures that if a key does not exist, it is automatically assigned an empty list ([]).
         anagrams = defaultdict(list)
 
         for word in strs:
             freq = [0] * 26  # Frequency array for 'a' to 'z'
             for char in word:
                 freq[ord(char) - ord('a')] += 1  # Count occurrences
-            
-            freq_key = tuple(freq)  # Convert list to string
+            # Tuple keys are immutable and correctly represent frequency distributions without unintended transformations
+            freq_key = tuple(freq)  # Convert list to tuple which hashable and maintains order correctly):
             anagrams[freq_key].append(word)  # Use string representation as key
         
         return list(anagrams.values())
