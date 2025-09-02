2796491264be214429d26af5d69a28d3cbe5efd9
Snehal Chaudhari <snehalachaudhari8@gmail.com>
2025-09-02 02:35:00 +0000

    Sync LeetCode submission Runtime - 10 ms (39.81%), Memory - 71.2 MB (45.30%)

---
+++ solutions/0238-product-of-array-except-self/solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let n= nums.length;
+    let ans= new Array(n).fill(1);
+    let prefix = new Array(n).fill(1);
+    let suffix= new Array(n).fill(1);
+
+    for(let i=1; i< n; i++){
+        prefix[i] = prefix[i-1]*nums[i-1];
+    }
+
+    for(let i= n- 2; i>=0; i--){
+        suffix[i]= suffix[i+1]*nums[i+1];
+    }
+
+    for(let i=0; i<n; i++){
+        ans[i]= prefix[i]* suffix[i];
+    }
+
+    return ans;
+
+};
